2aed5903a889c1e9e26ef7ebec25fdd4707dc80a
Jevin Anderson <jevin.anderson@recruitingventures.com>
2016-03-31 14:12:30 -0700

    Initial commit
    Added paging component

---
+++ src/Components.js
@@ -0,0 +1,4 @@
+import Pagination from './components/Pagination';
+
+export { Pagination };
+
---
+++ src/components/Pagination.js
@@ -0,0 +1,108 @@
+import React from 'react';
+
+class Pagination extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.changePage = this.changePage.bind(this);
+    this.changePageSize = this.changePageSize.bind(this);
+    this.createPageLinks = this.createPageLinks.bind(this);
+    this.getSelectabilityClass = this.getSelectabilityClass.bind(this);
+    this.number_of_paginators = props.numberOfPaginators || 7;
+  }
+
+  getSelectabilityClass(page) {
+    if (page < 1 || page > this.calculateLastPage() || page === this.props.page) {
+      return 'not-selectable';
+    } else {
+      return 'selectable';
+    }
+  }
+
+  changePageSize(e) {
+    const new_page_size = parseInt(e.currentTarget.value, 10);
+    const first_record_on_page = (this.props.page * this.props.pageSize) - (this.props.pageSize - 1);
+    const new_page = Math.ceil(first_record_on_page / new_page_size);
+    this.props.onPageSizeChange && this.props.onPageSizeChange(new_page_size, new_page);
+  }
+
+  changePage(page) {
+    if (page !== this.props.page && page > 0 && page <= this.calculateLastPage()) {
+      this.props.onPageChange && this.props.onPageChange(page);
+    }
+  }
+
+  calculateStartPage(total_pages) {
+    let start_page = 1;
+    if (total_pages > this.number_of_paginators && this.props.page > Math.ceil(this.number_of_paginators / 2)) {
+      if (this.props.page > total_pages - Math.floor(this.number_of_paginators / 2)) {
+        start_page = total_pages - this.number_of_paginators + 1;
+      } else {
+        start_page = this.props.page - Math.floor(this.number_of_paginators / 2);
+      }
+    }
+    return start_page;
+  }
+
+  calculateEndPage(start_page, total_pages) {
+    return total_pages > this.number_of_paginators ? start_page + (this.number_of_paginators - 1) : total_pages;
+  }
+
+  calculateLastPage() {
+    return this.props.total ? Math.ceil(this.props.total / this.props.pageSize) : 1;
+  }
+
+  createPageLinks(total_pages) {
+    const start_page = this.calculateStartPage(total_pages);
+    const end_page = this.calculateEndPage(start_page, total_pages);
+    const page_links = [];
+
+    for (let page = start_page; page <= end_page; page++) {
+      page_links.push(<a className={`page-link ${page === this.props.page ? 'current-page' : ''} ${this.getSelectabilityClass(page)}`} key={page} onClick={this.changePage.bind(this, page)}>{page}</a>);
+    }
+    return page_links;
+  }
+
+  render() {
+    const prev_page = this.props.page - 1;
+    const next_page = this.props.page + 1;
+    const last_page = this.calculateLastPage();
+    const page_links = this.createPageLinks(last_page);
+    const page_options = this.props.pageSizeOptions.map(value => <option key={value} value={value}>{value}</option>);
+    return (
+      <div className="pagination">
+        <select className="page-size" defaultValue={this.props.pageSize} onChange={this.changePageSize}>
+          {page_options}
+        </select>
+        <div className="page-links">
+          <a className={`page-link page-link-first ${this.getSelectabilityClass(1)}`} onClick={this.changePage.bind(this, 1)}>first</a>
+          <a className={`page-link page-link-previous ${this.getSelectabilityClass(prev_page)}`} onClick={this.changePage.bind(this, prev_page)}>prev</a>
+          {page_links}
+          <a className={`page-link page-link-next ${this.getSelectabilityClass(next_page)}`} onClick={this.changePage.bind(this, next_page)}>next</a>
+          <a className={`page-link page-link-last ${this.getSelectabilityClass(last_page)}`} onClick={this.changePage.bind(this, last_page)}>last</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+Pagination.propTypes = {
+  pageSize: React.PropTypes.number,
+  pageSizeOptions: React.PropTypes.array,
+  page: React.PropTypes.number,
+  total: React.PropTypes.number,
+  onPageSizeChange: React.PropTypes.func,
+  onPageChange: React.PropTypes.func,
+  numberOfPaginators: React.PropTypes.number
+};
+
+Pagination.defaultProps = {
+  pageSize: 10,
+  pageSizeOptions: [10, 25, 50, 100, 250],
+  page: 1,
+  total: 100,
+  numberOfPaginators: 10
+};
+
+export default Pagination;
+
